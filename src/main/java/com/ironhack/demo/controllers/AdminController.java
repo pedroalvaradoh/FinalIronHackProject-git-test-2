3b35986dc514a97a731650eb05c3ffd51132e91a
Nathalia Aristigueta <aristiguetanathalia@gmail.com>
2023-05-15 21:35:57 +0200

    commit falta relacion entre tablas admin y user

--- src/main/java/com/ironhack/demo/controllers/AdminController.java
+++ src/main/java/com/ironhack/demo/controllers/AdminController.java
@@ -1,9 +1,12 @@
 package com.ironhack.demo.controllers;
 
 import com.ironhack.demo.dto.ActivityDTO;
+import com.ironhack.demo.dto.UserDTO;
 import com.ironhack.demo.models.Activity;
+import com.ironhack.demo.models.User;
 import com.ironhack.demo.services.ActivityService;
 import com.ironhack.demo.services.AdminService;
+import com.ironhack.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,22 +20,42 @@ public class AdminController {
     ActivityService activityService;
     @Autowired
     AdminService adminService;
+    @Autowired
+    UserService userService;
 
-    @GetMapping ("/manage-activity")
-    @ResponseStatus (HttpStatus.OK)
-    public List<Activity> activityList(){
+    @GetMapping("/manage-activity")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Activity> activityList() {
         return activityService.activityList();
     }
 
-    @PostMapping ("/add-activity")
-    @ResponseStatus (HttpStatus.CREATED)
-    public Activity postActivity (@RequestBody Activity activity) {
+    @PostMapping("/add-activity")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Activity postActivity(@RequestBody Activity activity) {
         return adminService.addActivity(activity);
     }
 
-    @PatchMapping ("/update-activity-status")
+    @PatchMapping("/update-activity-status")
     @ResponseStatus(HttpStatus.OK)
     public Activity patchActivity(@RequestBody ActivityDTO activityDTO) {
         return adminService.updateActivity(activityDTO);
+    }
+
+    @DeleteMapping("/delete-activityyy/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAct(@PathVariable Long id) {
+        adminService.deleteActivity(id);
+    }
+
+    @GetMapping("/check-user")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> userList() {
+        return adminService.checkUser();
+    }
+
+    @PutMapping ("/update-user/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List <User> putUser(@RequestBody UserDTO userDTO) {
+        return null;
     }
 }
--- src/main/java/com/ironhack/demo/services/AdminService.java
+++ src/main/java/com/ironhack/demo/services/AdminService.java
@@ -1,10 +1,13 @@
 package com.ironhack.demo.services;
 
 import com.ironhack.demo.dto.ActivityDTO;
+import com.ironhack.demo.dto.UserDTO;
 import com.ironhack.demo.models.Activity;
 import com.ironhack.demo.models.Instructor;
+import com.ironhack.demo.models.User;
 import com.ironhack.demo.repositories.ActivityRepository;
 import com.ironhack.demo.repositories.AdminRepository;
+import com.ironhack.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -19,6 +22,9 @@ public class AdminService {
 
     @Autowired
     ActivityRepository activityRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     public List <Activity> activities(){
         return activityRepository.findAll();
@@ -35,5 +41,18 @@ public class AdminService {
         activity.setActivityStatus(activityDTO.getActivityStatus());
         return activityRepository.save(activity);
     }
+
+    public void deleteActivity(Long id) {
+        activityRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Activity does not exist"));
+        activityRepository.deleteById(id);
+    }
+
+    public List <User> checkUser(){
+        return userRepository.findAll();
+    }
+
+    //public User user (UserDTO userDTO) {
+    //    User user = userRepository.findAllById(UserDTO.ge)
+    //}
 
 }
